e452c1dd959648cd01a34c10cc4efa2a7c44acac
Lucas <lucassoares2007@hotmail.com>
2023-01-26 18:53:02 -0300

    perf

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-algo = input('Digite algo: ')
-print(f'O tipo primitivo desse valor é {type(algo)}')
-print(f'Só tem espaços? {algo.isspace()}')
-print(f'É um número? {algo.isnumeric()}')
-print(f'É alfabetico? {algo.isalpha()}')
-print(f'É alfanumerico? {algo.isalnum()}')
-print(f'Está em maiúsculas? {algo.isupper()}')
-print(f'Está em minúsculas? {algo.islower()}')
-print(f'Está capitalizada? {algo.istitle()}')
+n1 = float(input('Primeira nota do aluno: '))
+n2 = float(input('Segunda nota do aluno: '))
+media = (n1+n2)/2
+print(f'A média entre {n1:.1f} e {n2:.1f} é igual a {media:.1f}')
