c80e971f9670c2dfa8e9680210a61d2769e48432
Lucas <lucassoares2007@hotmail.com>
2023-01-27 20:24:03 -0300

    refactor

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-from playsound import playsound
-playsound('main.mp3')
+from random import shuffle
+a1 = str(input('Primeiro aluno: '))
+a2 = str(input('Segundo aluno: '))
+a3 = str(input('Terceiro aluno: '))
+a4 = str(input('Quarto aluno: '))
+alunos = [a1, a2, a3, a4]
+shuffle(alunos)
+print(f"""A ordem de apresentação será
+{alunos}""")
