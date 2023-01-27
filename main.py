93a966d034ef6ee7f1ca71b7e9928b0ebd34c5d1
Lucas <lucassoares2007@hotmail.com>
2023-01-27 03:16:50 -0300

    perf

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from random import choice
+from random import shuffle
 a1 = str(input('Primeiro aluno: '))
 a2 = str(input('Segundo aluno: '))
 a3 = str(input('Terceiro aluno: '))
 a4 = str(input('Quarto aluno: '))
 alunos = [a1, a2, a3, a4]
-print(f'O aluno escolhido foi {choice(alunos)}')
+shuffle(alunos)
+print(f"""A ordem de apresentação será
+{alunos}""")
