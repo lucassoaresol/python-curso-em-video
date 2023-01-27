714e32fe6532b26ab6d76997dfcd54e9ac3e5a43
Lucas <lucassoares2007@hotmail.com>
2023-01-27 01:06:18 -0300

    perf

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-nome = input('Qual seu nome ?')
-print(f'Olá, {nome}, seja bem-vindo!')
+temperatura_celsius = float(input('Informe a temperatura em ºC: '))
+temperatura_fahrenheit = (9/5)*temperatura_celsius + 32
+print(f'A temperatura de {temperatura_celsius:.1f}ºC corresponde a {temperatura_fahrenheit:.1f}ºF!')
+
