aa9a4ef3fe470de171fdfa8d53328a115b8b7269
Lucas <lucassoares2007@hotmail.com>
2023-01-28 19:13:22 -0300

    perf

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-cidade = str(input('Em que cidade você nasceu? ')).strip()
-cidade_lista = cidade.split()
-resposta = cidade_lista[0].upper() == 'SANTO'
-print(f'{resposta}')
+nome = str(input('Qual é seu nome completo? ')).upper()
+lista_nome = nome.split()
+compare = 'SILVA' in lista_nome
+print(f'Seu nome tem Silva? {compare}')
