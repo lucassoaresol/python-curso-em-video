d17ebc48a7f77c62ae4d267493e575e70e6e031b
Lucas <lucassoares2007@hotmail.com>
2023-01-31 19:02:11 -0300

    perf

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-nome = str(input('Digite seu nome completo: ')).strip().split()
-print(f"""Muito prazer em te conhecer!
-Seu primeiro nome é {nome[0]}
-Seu último nome é {nome[-1]}""")
+from random import randint
+from time import sleep
+
+computador = randint(0, 5)
+print("-=-" * 20)
+print("Vou pensar em um número entre 0 e 5. Tente adivinhar...")
+print("-=-" * 20)
+jogador = int(input("Em que número eu pensei? "))
+print("PROCESSANDO...")
+sleep(3)
+if jogador == computador:
+    print("PARABÉNS! Você conseguiu me vencer!")
+else:
+    print(f"GANHEI! Eu pensei no número {computador} e não no {jogador}!")
